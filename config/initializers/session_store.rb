df94bf4163efa8cb697d60e93eaa89ea859d8e2f
Brent Pellinen <brent.pellinen@state.mn.us>
2024-04-28 08:43:13 -0500

    set session length to 60 minutes

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,4 @@
+Rails.application.config.session_store :cookie_store,
+                                       key: '_fvadmin',
+                                       same_site: :lax,
+                                       expire_after: 60.minutes
