dd32238bd9f9e583a81744a3507e0b00e4df5680
Brent Pellinen <bpellinen@gmail.com>
2024-11-24 20:49:32 -0600

    cleanup oidc config

--- config/initializers/oidc.rb
+++ config/initializers/oidc.rb
@@ -2,7 +2,13 @@ require 'openid_connect'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 # OpenIDConnect.debug! if Rails.env.development?
-  unless [ENV['OP_HOST'], ENV['OP_CLIENT_ID'], ENV['OP_SECRET_KEY'], ENV['OP_REDIRECT_URI']].all?(&:present?)
+  host = ENV['OP_HOST']
+  client_id = ENV['OP_CLIENT_ID']
+  secret = ENV['OP_SECRET_KEY']
+  realm = ENV['OP_REALM']
+  redirect = ENV['OP_REDIRECT_URI']
+
+  unless [host, client_id, secret, redirect].all?(&:present?)
     raise "Configuration error. OP_HOST, OP_CLIENT_ID, OP_SECRET_KEY, or OP_REDIRECT_URI variables not set."
   end
 
@@ -10,16 +16,16 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     name: :oidc,
     scope: [:openid, :email, :profile, :address],
     response_type: :code,
-    'issuer' => "https://#{ENV['OP_HOST']}#{ENV['OP_REALM']}",
+    'issuer' => "https://#{host}#{realm}",
     uid_field: 'preferred_username',
     client_options: {
-      host: ENV['OP_HOST'],
-      identifier: ENV["OP_CLIENT_ID"],
-      secret: ENV["OP_SECRET_KEY"],
-      authorization_endpoint: "#{ENV['OP_REALM']}#{ENV['OP_AUTH_ENDPOINT']}",
-      token_endpoint: "#{ENV['OP_REALM']}#{ENV['OP_TOKEN_ENDPOINT']}",
-      userinfo_endpoint: "#{ENV['OP_REALM']}#{ENV['OP_USERINFO_ENDPOINT']}",
-      jwks_uri: "https://#{ENV['OP_HOST']}#{ENV['OP_REALM']}#{ENV['OP_JWKS_ENDPOINT']}",
+      host: host,
+      identifier: client_id,
+      secret: secret,
+      authorization_endpoint: "#{realm}#{ENV['OP_AUTH_ENDPOINT']}",
+      token_endpoint: "#{realm}#{ENV['OP_TOKEN_ENDPOINT']}",
+      userinfo_endpoint: "#{realm}#{ENV['OP_USERINFO_ENDPOINT']}",
+      jwks_uri: "https://#{host}#{realm}#{ENV['OP_JWKS_ENDPOINT']}",
       redirect_uri: ENV['OP_REDIRECT_URI'],
     },
   }
